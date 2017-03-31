ac236c1e74b67341a58f28527b7a20b858bd83f5
unknown <sukanta.chakraborty@LMKOLPC150.learningmatekol.local>
2017-03-31 18:39:54 +0530

    second commit

--- book.js
+++ book.js
@@ -1,38 +1,63 @@
-var count = 0;
-$(document).ready(function() {
-    addBook();
-});
 
-function addBook() {
-    $('.btn').click(function() {
-        count++;
+var allRecord = [];
 
-        var Booktitle = $('#btl').val();
-        author = $('#auth').val();
-        page = $('#pg').val();
-        pdate = $('#pd').val();
-        $("#btl").val('');
-        $('#auth').val('');
-        $('#pg').val('');
-        $('#pd').val('');
-        //  console.log(Booktitle);
+var addbutton = document.getElementsByClassName('btn');
 
 
+ // console.log(removebutton);
+window.onload = initialise();
+//Initialize when window is loading/refreshing
+function initialise() {
+    addbutton[0].addEventListener('click', inputValueFetch);
 
-        // if (window.localStorage) {
-        //
-        //     // Save data to the current local store
-        //
-        //     localStorage.setItem("bookname",Booktitle );
-        //
-        //     // Access some stored data
-        //     console.log("bookname = " + localStorage.getItem("bookname"));
-        //
-        // } else {
-        //     console.log("local storage not supported");
-        // }
+    allRecord = JSON.parse(localStorage.getItem('mainArray')) ? JSON.parse(localStorage.getItem('mainArray')) : [];
 
-          $('.table').append("<tr><td>" + Booktitle + "</td><td>" + author + "</td><td>" + page + "</td><td>" + pdate + "</td></tr>");
-    });
+    for (var i = 0; i < allRecord.length; i++) {
+        document.getElementsByClassName('table')[0].innerHTML += "<tr><td>" + allRecord[i].booktitle + "</td><td>" + allRecord[i].author + "</td><td>" + allRecord[i].page + "</td><td>" + allRecord[i].pdate + "</td><td><button  class='btn btn-danger'>" + "remove" + "</button></td></tr>";
+    }
+}
+//Parsing Input
+function inputValueFetch() {
+  var tableRecord = {
+      "booktitle": "",
+      "author": "",
+      "page": "",
+      "pdate": ""
+  };
+    booktitle = document.getElementById('booktitle').value;
+    author = document.getElementById('author').value;
+    page = document.getElementById('page').value;
+    publishdate = document.getElementById('publishdate').value;
+
+    document.getElementById('booktitle').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('page').value = '';
+    document.getElementById('publishdate').value = '';
+
+    document.getElementsByClassName('table')[0].innerHTML += "<tr><td>" + booktitle + "</td><td>" + author + "</td><td>" + page + "</td><td>" + publishdate + "</td><td><button  class='btn btn-danger'>" + "remove" + "</button></td><td><button  class='btn btn-danger'>" + "remove" + "</button></td></tr>";
+var removebutton=document.getElementsByClassName('btn-danger');
+    tableRecord.booktitle =  booktitle;
+    tableRecord.author =author;
+    tableRecord.page = page;
+    tableRecord.pdate = publishdate;
+    allRecord.push(tableRecord);
+    localStorage.setItem("mainArray", JSON.stringify(allRecord));
+    for(var i=0;i<removebutton.length;i++){
+    removebutton[i].addEventListener('click', confirmDelete);
+  }
+
+
+
+
 
 }
+  function confirmDelete(){
+    var txt;
+    var r = confirm("Press a button!");
+    if (r === true) {
+      // localStorage.removeItem()
+    } else {
+        txt = "You pressed Cancel!";
+    }
+
+  }
