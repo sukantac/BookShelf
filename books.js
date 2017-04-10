542097ac3575fa0a685845f15d0246b25adfcd27
unknown <sukanta.chakraborty@LMKOLPC150.learningmatekol.local>
2017-04-10 17:59:48 +0530

    third commit

--- books.js
+++ books.js
@@ -1,57 +1,103 @@
-var bookShelf = {
-    "booktitle": "",
-    "author": "",
-    "page": "",
-    "pdate": "",
-    inputValueFetch: inputValueFetch
-};
-var title=[];
-var addbutton = document.getElementsByClassName('btn');
+var addbutton = document.getElementsByClassName('btn'),
+    bookobj;
+var allRecord = [];
+var removebutton;
 
-window.onload = initialise();
+function Book() {}
 
-function initialise() {
-    addbutton[0].addEventListener('click', bookShelf.inputValueFetch);
-    // document.getElementsByClassName('table')[0].innerHTML += "<tr><td>" + bookDetails + "</td></tr>";
-    var bookDetails = localStorage.getItem("Booktitle");
-    var authorname = localStorage.getItem("Author");
-    var pagenumber = localStorage.getItem("Page");
-    var publicationdate = localStorage.getItem("Publishdate");
-    document.getElementsByClassName('table')[0].innerHTML += "<tr><td>" + bookDetails + "</td><td>" + authorname + "</td><td>" + pagenumber + "</td><td>" + publicationdate + "</td></tr>";
+function GetterSetter() {}
+window.addEventListener('load', init);
+
+function init() {
+    bookobj = new Book();
+    getterSetter = new GetterSetter();
+    addbutton[0].addEventListener('click', getterSetter.store);
+    getterSetter.retrieve();
+    removebutton = document.getElementsByClassName('btn-danger');
+    for (var k = 0; k < removebutton.length; k++) {
+        removebutton[k].removeEventListener('click', confirmDelete);
+        removebutton[k].addEventListener('click', confirmDelete);
+    }
+
 }
-// var parseInput = function(){
-//     this.booktitle = document.getElementById('booktitle').value;
-//     this.author = document.getElementById('author').value;
-//     this.page = document.getElementById('page').value;
-//     this.publishdate = document.getElementById('publishdate').value;
-// }
-// var parsing = new parseInput();
+Book.prototype.book = {};
+Book.prototype.setTitle = function(title, Book) {
+    this.book.title = title;
+}
+Book.prototype.setAuthor = function(author, Book) {
+    this.book.author = author;
+}
+Book.prototype.setPage = function(page, Book) {
+    this.book.page = page;
+}
+Book.prototype.setPublish = function(publish, Book) {
+    this.book.publish = publish;
+}
+Book.prototype.setIdentity = function(identity, Book) {
+    this.book.identity = identity;
+}
 
-function inputValueFetch() {
 
-    this.booktitle = document.getElementById('booktitle').value;
-    title.push(this.booktitle);
-    console.log(title);
+GetterSetter.prototype.store = function() {
+    this.rowId = Date.now();
+    this.bookTitle = document.getElementById('booktitle').value;
+
     this.author = document.getElementById('author').value;
+
     this.page = document.getElementById('page').value;
-    this.publishdate = document.getElementById('publishdate').value;
 
-  //  var myString= JSON.stringify(title);
+    this.publishDate = document.getElementById('publishdate').value;
 
-    localStorage.setItem("Booktitle", title);
-    localStorage.setItem("Author", this.author);
-    localStorage.setItem("Page", this.page);
-    localStorage.setItem("Publishdate", this.publishdate);
-    document.getElementById('booktitle').value = '';
-    document.getElementById('author').value = '';
-    document.getElementById('page').value = '';
-    document.getElementById('publishdate').value = '';
-    //  console.log(Booktitle);
+    document.getElementById('booktitle').value = "";
+    document.getElementById('author').value = "";
+    document.getElementById('page').value = "";
+    document.getElementById('publishdate').value = "";
+    bookobj.setTitle(this.bookTitle, bookobj);
+    bookobj.setAuthor(this.author, bookobj);
+    bookobj.setPage(this.page, bookobj);
+    bookobj.setPublish(this.publishDate, bookobj);
+    bookobj.setIdentity(this.rowId, bookobj);
+    allRecord.push(bookobj.book);
+    localStorage.setItem("mainArray", JSON.stringify(allRecord));
+    tableCreation([bookobj.book]);
+    removebutton = document.getElementsByClassName('btn-danger');
+    for (var k = 0; k < removebutton.length; k++) {
+        removebutton[k].removeEventListener('click', confirmDelete);
+        removebutton[k].addEventListener('click', confirmDelete);
+    }
+    bookobj.book = {};
+}
 
-    //  $('.table').append("<tr><td>" + booktitle + "</td><td>" + author + "</td><td>" + page + "</td><td>" + pdate + "</td></tr>");
-    // document.getElementsByClassName('table')[0].innerHTML += "<tr><td>" + this.booktitle + "</td><td>" + this.author + "</td><td>" + this.page + "</td><td>" + this.pdate + "</td></tr>"
-    document.getElementsByClassName('table')[0].innerHTML += "<tr><td>" + title + "</td><td>" + this.author + "</td><td>" + this.page + "</td><td>" + this.publishdate + "</td></tr>";
-    // console.log(booktitle);
+GetterSetter.prototype.retrieve = function() {
+    allRecord = JSON.parse(localStorage.getItem('mainArray')) ? JSON.parse(localStorage.getItem('mainArray')) : [];
+    if (allRecord.length) {
 
-    // localstorageValueSet(this.booktitle);
+        tableCreation(allRecord);
+    }
+}
+
+function tableCreation(bookDeatils) {
+    for (var i = 0; i < bookDeatils.length; i++) {
+        document.querySelectorAll('table>tbody')[0].innerHTML += "<tr><td>" + bookDeatils[i].title +
+            "</td><td>" + bookDeatils[i].author + "</td><td>" + bookDeatils[i].page +
+            "</td><td>" + bookDeatils[i].publish +
+            "</td><td><button  class='btn btn-danger'  id='book" + bookDeatils[i].identity + "'>" + "remove" + "</button></td></tr>";
+    }
+
+}
+
+function confirmDelete() {
+    var storage = JSON.parse(localStorage.getItem('mainArray'));
+    var r = confirm("Sure to delete");
+    if (r === true) {
+        for (var z = 0; z < storage.length; z++) {
+            var buttonId = "book" + storage[z].identity;
+            if (buttonId === this.id) {
+
+                allRecord.splice(z, 1);
+                localStorage.setItem("mainArray", JSON.stringify(allRecord));
+                this.parentNode.parentNode.remove();
+            }
+        }
+    }
 }
